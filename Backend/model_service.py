17026a9f43d9916654e5811a023a7721e57c78f7
dangduong2806 <160890193+dangduong2806@users.noreply.github.com>
2025-05-25 12:02:06 +0700

    Delete Backend directory

--- Backend/model_service.py
+++
@@ -1,6 +0,0 @@
-from deepface import DeepFace
-def predict(image_path):
-    check = DeepFace.find(img_path=image_path,db_path="/web_service/people")
-    if len(check[0]) > 0:
-        return True
-    return False
